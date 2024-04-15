4b2a14fc8f9a95584ef62ddcecd2ac8ed69ae809
NithyasriVS <nithyasri.srivathsan.iyengar@gmail.com>
2024-04-15 15:36:57 +0800

    add pseudocode in future

--- tebd.py
+++ tebd.py
@@ -4,13 +4,11 @@ import numpy as np
 # pseudocode for tebd
 
 dt = 1e-2
-np.exp(1j * dt)
+u = np.exp(1j * dt)
 
 # unitaries -> internally commuting parts
-# exponentiate the part individually
-# product them to get an MPO
-# svd decompose MPO
-# repeat svd
+# individually exponentiate the parts
+# ???
 # apply U repeatedly as per time interval
 # final mps after evol
 # dense vector of final mps
