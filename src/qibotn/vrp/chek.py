aa69b766295c242d85a9237478e81712f53f661f
NithyasriVS <nithyasri.srivathsan.iyengar@gmail.com>
2025-01-13 23:28:14 +0800

    chek file is the complete qubo formulation and hamiltonian creation - running on cpu vqe and produced results

---
+++ src/qibotn/vrp/chek.py
@@ -0,0 +1,112 @@
+from qubo_utils import binary2spin, spin2QiboHamiltonian
+import qibo
+from qibo import Circuit, models, gates
+import numpy as np
+
+ncust = 4
+
+'''distance_matrix= [
+    [0.0, 9.849096, 8.29975427, 10.34143689, 5.27563503],
+    [9.849096, 0.0, 2.81998316, 0.71700279, 5.80897728],
+    [8.29975427, 2.81998316, 0.0, 2.81583664, 5.78378993],
+    [10.34143689, 0.71700279, 2.81583664, 0.0, 6.47844441],
+    [5.27563503, 5.80897728, 5.78378993, 6.47844441, 0.0]
+]'''
+
+distance_matrix = [
+    [0.0, 1.0, 2.0, 3.0],
+    [1.0, 0.0, 4.0, 5.0],
+    [2.0, 6.0, 0.0, 1.0],
+    [3.0, 4.0, 2.0, 0.0] 
+]
+
+def build_qubo(distance_matrix, ncust):
+    lin_qubo = {}
+    quad_qubo = {}
+
+    def find_ind(i, j, ncust): 
+        return i * ncust + j  # Map 2D index (i, j) to a 1D index
+
+    # Constructing the QUBO terms
+    for i in range(ncust):
+        for j in range(ncust):
+            if i != j:  # Skip self-loops
+                var_index = find_ind(i, j, ncust)
+                
+                if var_index not in lin_qubo:
+                    lin_qubo[var_index] = 0
+                lin_qubo[var_index] += distance_matrix[i][j]
+
+    penalty = 100  # Penalty for violating constraints
+
+    # Add constraints (each point must be entered and exited exactly once)
+    for i in range(ncust):
+        for j in range(ncust):
+            if i != j:
+                for k in range(ncust):
+                    if k != j and k != i:
+                        var1, var2 = find_ind(i, j, ncust), find_ind(i, k, ncust)
+                        if var1 == var2:
+                            if var1 not in lin_qubo:
+                                lin_qubo[var1] = 0
+                            lin_qubo[var1] += penalty
+                        else:
+                            if (var1, var2) not in quad_qubo:
+                                quad_qubo[(var1, var2)] = 0
+                            quad_qubo[(var1, var2)] += penalty
+
+        # Each point must be entered exactly once
+        for j in range(ncust):
+            if i != j:
+                for k in range(ncust):
+                    if k != i and k != j:
+                        var1, var2 = find_ind(j, i, ncust), find_ind(k, i, ncust)
+                        if var1 == var2:
+                            if var1 not in lin_qubo:
+                                lin_qubo[var1] = 0
+                            lin_qubo[var1] += penalty
+                        else:
+                            if (var1, var2) not in quad_qubo:
+                                quad_qubo[(var1, var2)] = 0
+                            quad_qubo[(var1, var2)] += penalty
+
+    # Start and end at the depot (point 0)
+    for j in range(1, ncust):
+        start_var = find_ind(0, j, ncust)
+        end_var = find_ind(j, 0, ncust)
+        if start_var not in lin_qubo:
+            lin_qubo[start_var] = 0
+        lin_qubo[start_var] += penalty
+        if end_var not in lin_qubo:
+            lin_qubo[end_var] = 0
+        lin_qubo[end_var] += penalty
+
+    return lin_qubo, quad_qubo
+
+
+# Generate the QUBO from the distance matrix
+lin_qubo, quad_qubo = build_qubo(distance_matrix, ncust)
+
+#print("QUBO Linear Terms:", lin_qubo)
+#print("QUBO Quadratic Terms:", quad_qubo)
+
+linear, quadratic = build_qubo(distance_matrix, ncust)
+
+h, J, _ = binary2spin(linear, quadratic)
+h = {k: -v for k, v in h.items()}
+J = {k: -v for k, v in J.items()}
+
+ham = spin2QiboHamiltonian(h, J, dense=False)
+
+print(ham, type(ham))
+
+nqubits = 15
+c = Circuit(nqubits)
+for i in range(0, nqubits):
+    c.add(gates.RX(i,0))
+
+test_vqe = models.VQE(c, ham)
+initial_parameters=np.random.uniform(0, 2 * np.pi, 15)
+
+print(test_vqe.minimize(initial_parameters))
+
--- src/qibotn/vrp/main.py
+++ src/qibotn/vrp/main.py
@@ -10,7 +10,7 @@ from qubo_utils import binary2spin, spin2QiboHamiltonian
     qubo_vrp_penalty
 )'''
 
-def load_vrp(filename):
+def load_problem(filename):
     """Load vrp problem from a file
 
     The file format is compatible with the one used in QAPLIB without considering Flow
@@ -18,32 +18,104 @@ def load_vrp(filename):
     """
 
     with open(filename) as fh:
+        computed_distances = []
         instance_size = int(fh.readline())
+        #computed_distances = [float(instance_size) for instance_size in fh.read().split()]
+        for l in fh:
+            computed_distances_row = [float(x) for x in l.split()]
+            computed_distances.append(computed_distances_row)
+    
+    return computed_distances, instance_size
 
-        d = [float(instance_size) for instance_size in fh.read().split()]
+def build_qubo(distance_matrix, ncust):
+    
+    lin_qubo = {}
+    quad_qubo = {}
 
-    return d
+    def find_ind(i,j): return i * ncust + j
 
-def qubo_vrp(D, p):
+    for i in range(ncust):
+        for j in range(ncust):
+            if i != j: # AVOID i to i (seof loops)
+                var_index = find_ind(i, j)
+                if var_index not in lin_qubo:
+                    lin_qubo[var_index] = 0  # Initialize if not already present
+                lin_qubo[var_index] += distance_matrix[i][j]
+
+    penalty= 100
+    for i in range(ncust):
+        # Each point must be exited exactly once
+        for j in range(ncust):
+            if i != j:
+                for k in range(ncust):
+                    if k != j and k != i:
+                        var1, var2 = find_ind(i, j), find_ind(i, k)
+                        if var1 == var2:
+                            if var1 not in lin_qubo:
+                                lin_qubo[var1] = 0
+                            lin_qubo[var1] += penalty
+                        else:
+                            if (var1, var2) not in quad_qubo:
+                                quad_qubo[(var1, var2)] = 0
+                            quad_qubo[(var1, var2)] += penalty
+
+        # Each point must be entered exactly once
+        for j in range(ncust):
+            if i != j:
+                for k in range(ncust):
+                    if k != i and k != j:
+                        var1, var2 = find_ind(j, i), find_ind(k, i)
+                        if var1 == var2:
+                            if var1 not in lin_qubo:
+                                lin_qubo[var1] = 0
+                            lin_qubo[var1] += penalty
+                        else:
+                            if (var1, var2) not in quad_qubo:
+                                quad_qubo[(var1, var2)] = 0
+                            quad_qubo[(var1, var2)] += penalty
+
+    # Start and end at the depot (point 0)
+    for j in range(1, ncust):
+        start_var = find_ind(0, j)
+        end_var = find_ind(j, 0)
+        if start_var not in lin_qubo:
+            lin_qubo[start_var] = 0
+        lin_qubo[start_var] += penalty
+        if end_var not in lin_qubo:
+            lin_qubo[end_var] = 0
+        lin_qubo[end_var] += penalty
+
+    return lin_qubo, quad_qubo
+
+def build_ham():
+
+    dm, size = load_problem("src/qibotn/vrp/data.txt")
+    linear, quadratic = build_qubo(dm, size)
+
+    h, J, _ = binary2spin(linear, quadratic)
+    h = {k: -v for k, v in h.items()}
+    J = {k: -v for k, v in J.items()}
+
+    ham = spin2QiboHamiltonian(h, J)
+
+    return ham
+
+'''def qubo_vrp(D, p):
     m = len(D)
-    qubo = np.zeros((m**2, m**2), dtype=np.float32)
+    qubo = np.zeros((m), dtype=np.float32)
     
     offset = p * 2 * m
-    for x in range(m):
-        for y in range(m):
-            # flatten into 1D
-            qubo_element = x*m + y
-            qubo[qubo_element,qubo_element] = D[x][y]
+    for x in range(0,m):
+            qubo[x] = (D[x])
 
-    linear = {i: qubo[i, i] for i in range(qubo.shape[0])}
-    quadratic = {
-        (i, j): qubo[i, j] for j in range(qubo.shape[1]) for i in range(qubo.shape[0]) if i > j
-    }
+    linear = {i: qubo[i] for i in range(qubo.shape[0])}
+    #quadratic = {
+    #    (i): qubo[i] for j in range(qubo.shape[1]) for i in range(qubo.shape[0]) if i > j
+    #}
+    quadratic = {i: qubo[i] for i in range(qubo.shape[0])}
 
     return linear, quadratic, offset
 
-        
-    return qubo
 
 def hamiltonian_vrp(D, penalty):
     linear, quadratic, offset = qubo_vrp(D, penalty)
@@ -56,18 +128,11 @@ def hamiltonian_vrp(D, penalty):
 
     return hamiltonian
 
+'''
+def main():
 
-def main(filename):
-    print(f"Load distance matrices from {filename} and make a QUBO")
-    D = load_vrp(filename)
-
-    linear, quadratic, offset = qubo_vrp((D), penalty=0)
-
-    print("Construct a hamiltonian directly from distance matrix")
-    ham = hamiltonian_vrp((D), dense=False)
-
-    print("done.")
+    ham = build_ham()
+    print(ham, type(ham))
 
 if __name__ == "__main__":
-    main("tiny0502.dat")
-
+    main()
