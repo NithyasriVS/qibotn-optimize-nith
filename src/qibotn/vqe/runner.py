ef9dd7b7c0e62e8020f31c6ebd7d434b7f28c7bd
NithyasriVS-Classiq <nithyasri.srivathsan.iyengar@gmail.com>
2025-01-19 14:46:06 +0800

    Cleaned up code from indep-vqe branch: VQE feature for qibotn. Recommited on this new branch freshly for raising PR

---
+++ src/qibotn/vqe/runner.py
@@ -0,0 +1,26 @@
+import qibo
+from qibo import models, hamiltonians, Circuit, gates
+import numpy as np
+from vqe_tn import run_vqe
+
+computation_settings = {
+            "MPI_enabled": False,
+            "MPS_enabled": False,
+            "NCCL_enabled": False,
+            "expectation_enabled": False
+}
+
+qibo.set_backend(backend="qibotn", platform="cutensornet", runcard=computation_settings)
+
+nqubits = 4
+c = Circuit(nqubits)
+for i in range(0, nqubits):
+    c.add(gates.RX(i,0))
+
+initial_parameters = 0.01 * np.random.random(nqubits)
+
+ham = hamiltonians.XXZ(nqubits)
+
+vqe_circuit = run_vqe(c, ham, initial_parameters)
+result = vqe_circuit()
+print(result.state())
---
+++ src/qibotn/vqe/utils.py
@@ -0,0 +1,9 @@
+# Loss function for Variational Quantum Eigensolver
+def vqe_loss_qibotn(p, c, h):
+        def loss_return(p, c, h):
+                c.set_parameters(p)
+                result = h.backend.execute_circuit(c)
+                final_state = result.state()
+                final_state = final_state.get()
+                return h.expectation(final_state)
+        return loss_return
