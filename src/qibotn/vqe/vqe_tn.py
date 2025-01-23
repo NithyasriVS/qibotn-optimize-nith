33bbad13d69c76d7c99b530910a2a38f7a7487c5
NithyasriVS <nithyasri.srivathsan.iyengar@gmail.com>
2025-01-23 17:45:50 +0800

    .

--- src/qibotn/vqe/vqe_tn.py
+++ src/qibotn/vqe/vqe_tn.py
@@ -5,5 +5,5 @@ from qibo import models
 def run_vqe(circuit, hamiltonian, initial_parameters):
     
     vqe = models.VQE(circuit, hamiltonian)
-    vqe.minimize(initial_parameters, loss_func=vqe_loss_qibotn_2(initial_parameters, circuit, hamiltonian))
+    vqe.minimize(initial_parameters, loss_func=vqe_loss_qibotn(initial_parameters, circuit, hamiltonian))
     return vqe.circuit
